6e02e8d265cbe8c41210b959205e1db7e8b9ba15
MikiraSora <mikirasora0409@126.com>
2021-02-20 17:24:00 +0800

    add avalonia project

---
+++ TestAvalonia/Live2DViewer.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.OpenGL;
+using Avalonia.OpenGL.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAvalonia
+{
+    public class Live2DViewer : OpenGlControlBase
+    {
+        public Live2DViewer():base()
+        {
+
+        }
+
+        protected override void OnOpenGlRender(GlInterface gl, int fb)
+        {
+
+        }
+    }
+}
